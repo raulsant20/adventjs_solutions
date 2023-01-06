f92dfceeac131879179d1a8e8bca400c2bf768e4
Raul Santa Cruz <raulsantacruz0@gmail.com>
2023-01-05 19:47:49 -0500

    challenge 7 solution 2

--- reto7.js
+++ reto7.js
@@ -1,13 +1,16 @@
 function getGiftsToRefill(a1, a2, a3) {
+  a1 = a1.filter((e,i) => a1.indexOf(e) === i)
+  a2 = a2.filter((e,i) => a2.indexOf(e) === i)
+  a3 = a3.filter((e,i) => a3.indexOf(e) === i)
   let answer = []
   for(let i=0; i<a1.length; i++){
-    if(!a2.includes(a1[i]) && !a3.includes(a1[i]) && !answer.includes(a1[i])) answer.push(a1[i])
+    if(!a2.includes(a1[i]) && !a3.includes(a1[i])) answer.push(a1[i])
   }
   for(let i=0; i<a2.length; i++){
-    if(!a1.includes(a2[i]) && !a3.includes(a2[i]) && !answer.includes(a2[i])) answer.push(a2[i])
+    if(!a1.includes(a2[i]) && !a3.includes(a2[i])) answer.push(a2[i])
   }
   for(let i=0; i<a3.length; i++){
-    if(!a1.includes(a3[i]) && !a2.includes(a3[i]) && !answer.includes(a3[i])) answer.push(a3[i])
+    if(!a1.includes(a3[i]) && !a2.includes(a3[i]) ) answer.push(a3[i])
   }
   return answer
 }
